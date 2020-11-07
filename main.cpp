b54aa091a1a1dd2c4a71b1252d7662d793d4213f
Chise Hachiroku <73808043+c86-moe@users.noreply.github.com>
2020-11-07 23:17:48 +0800

    Housekeeping

--- main.cpp
+++ main.cpp
@@ -2,11 +2,7 @@
 	Name:		GCG-filesorter
 	Author:		Chise Hachiroku (C86.moe)
 	Email:		i@c86.moe
-	Description:	This program can select changed items in by comparing with an older copy and
-			
-			
-			
-			
+	Description:	This program can select changed items in by comparing with an older copy and		
 			copying the changed ones to a new folder.
 	Useage:		<program name> [New copy] [Destination of changed items] [Older copy]
 */
